72468e847b8fdc451685e91fd3d6f5e9f6a7dcf2
吉原央賀 <ouga@ouganomacbook-pro.local>
2019-06-04 19:06:09 +0900

    Initial Commit

---
+++ originalCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  originalCamera
+//
+//  Created by 吉原央賀 on 2019/06/04.
+//  Copyright © 2019 ouga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
